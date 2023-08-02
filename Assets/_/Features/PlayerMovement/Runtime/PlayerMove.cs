ba26d9787e0fbd56931918f16d08bf38c56888da
Gabor_Vilics <vilics@hotmail.com>
2023-08-02 21:11:50 +0200

    real initial commit

---
+++ Assets/_/Features/PlayerMovement/Runtime/PlayerMove.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    #region Unity API
+
+    
+
+    #endregion
+
+    #region Main Methods
+
+    public void GetMovementData()
+    {
+        
+    }
+
+    #endregion
+
+    #region Private and Protected
+
+    [SerializeField] private float _moveSpeed;
+
+    #endregion
+}
